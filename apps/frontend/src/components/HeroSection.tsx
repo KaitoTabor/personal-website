ebbbeb8605f1899cf267a208822c6fd96bf143fb
KaitoTabor <kaitoariyasu@gmail.com>
2025-08-24 15:20:40 -0400

    Phone size for the hero section

---
+++ apps/frontend/src/components/HeroSection.tsx
@@ -0,0 +1,43 @@
+import {Card, CardContent} from "@/components/ui/card";
+
+const HeroSection = () => {
+    return (
+        <div className="flex items-center justify-center relative z-10">
+            <Card className="rounded-none relative overflow-hidden w-[49vh] h-[70vh] md:w-[49vw] md:h-[70vw]">
+                <div className="flex items-center justify-center ">
+                    <Card className='rounded-none absolute overflow-hidden w-[40vh] h-[31.5vh] top-[4vh] md:w-[45vw] md:h-[31.5vw] md:top-[4vw]'
+                          style={{
+                              backgroundImage:"url('/Hero-image-ocean.jpg')",
+                              backgroundSize: "cover",
+                              backgroundPosition: "center",
+                          }}>
+                    </Card>
+                </div>
+                <div className="absolute top-[40vh] text-[8vh] font-kanji w-full flex justify-end right-[1vh] md:top-[40vw] md:text-[8vw] md:right-[2vw]">
+                    海飛
+                </div>
+                <div className="absolute text-[4vh] font-kanji w-full flex justify-end top-[35vh] left-[18vh] rotate-90 md:text-[4vw] md:top-[33vw] md:left-[20vw]">
+                    Kaito
+                </div>
+                <div className="absolute top-[63.5vh] text-[5vh] font-kanji w-full flex justify-start left-[1vh] md:top-[63.5vw] md:text-[5vw] md:left-[1vw]">
+                    ポートフォリオ
+                </div>
+                <div className="absolute top-[60vh] text-[4vh] font-kanji w-full flex justify-start left-[1vh] md:top-[60vw] md:text-[4vw] md:left-[1vw]">
+                    Portfolio
+                </div>
+                <div className="absolute top-[30vh] text-[4vh] font-kanji w-full flex justify-end right-[2vh] text-white md:top-[30vw] md:text-[4vw] md:right-[2vw]">
+                    WPI/JESGO
+                </div>
+                <div className="absolute top-[25vh] text-[4vh] font-kanji w-full flex justify-end right-[2vh] text-white md:top-[25vw] md:text-[4vw] md:right-[2vw]">
+                    CS&DS
+                </div>
+
+
+                <CardContent className="relative z-10 p-6">
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
+
+export default HeroSection;
--- apps/frontend/src/routes/Home.tsx
+++ apps/frontend/src/routes/Home.tsx
@@ -2,60 +2,36 @@ import React from 'react';
 import ExampleComponent from '../components/ExampleComponent.js';
 import {Card, CardContent} from "@/components/ui/card";
 import Waves from "@blocks/Backgrounds/Waves/Waves";
+import HeroSection from "@/components/HeroSection";
 
 const ExamplePage = () => {
     return (
-        <div className="relative z-0 p-10 min-h-screen min-w-screen">
-            <Waves
-                lineColor="rgba(0, 0, 0, 0.2)"
-                backgroundColor="rgba(16, 60, 101, 1)"
-                waveSpeedX={0.02}
-                waveSpeedY={0.01}
-                waveAmpX={40}
-                waveAmpY={20}
-                friction={0.9}
-                tension={0.01}
-                maxCursorMove={0}
-                xGap={12}
-                yGap={36}
-                style={{ pointerEvents: "none" }}>
-            </Waves>
-            <div className="flex items-center justify-center relative z-10">
-                <Card className="rounded-none relative overflow-hidden w-[70vh] h-[49vh] md:w-[49vw] md:h-[70vw]">
-                    <div className="flex items-center justify-center ">
-                        <Card className='rounded-none absolute overflow-hidden w-[31.5vh] h-[45vh] top-[4vh] md:w-[45vw] md:h-[31.5vw] md:top-[4vw]'
-                        style={{
-                            backgroundImage:"url('/Hero-image-ocean.jpg')",
-                            backgroundSize: "cover",
-                            backgroundPosition: "center",
-                        }}>
-                        </Card>
-                    </div>
-                    <div className="absolute top-[40vh] text-[8vh] font-kanji w-full flex justify-end right-[2vh] md:top-[40vw] md:text-[8vw] md:right-[2vw]"> 
-                        海飛
-                    </div>
-                    <div className="absolute text-[4vh] font-kanji w-full flex justify-end top-[33vh] left-[20vh] rotate-90 md:text-[4vw] md:top-[33vw] md:left-[20vw]"> 
-                        Kaito
-                    </div>
-                    <div className="absolute top-[63.5vh] text-[5vh] font-kanji w-full flex justify-start left-[1vh] md:top-[63.5vw] md:text-[5vw] md:left-[1vw]"> 
-                        ポートフォリオ
-                    </div>
-                    <div className="absolute top-[60vh] text-[4vh] font-kanji w-full flex justify-start left-[1vh] md:top-[60vw] md:text-[4vw] md:left-[1vw]"> 
-                        Portfolio
-                    </div>
-                    <div className="absolute top-[30vh] text-[4vh] font-kanji w-full flex justify-end right-[2vh] text-white md:top-[30vw] md:text-[4vw] md:right-[2vw]"> 
-                        WPI/JESGO
-                    </div>
-                    <div className="absolute top-[25vh] text-[4vh] font-kanji w-full flex justify-end right-[2vh] text-white md:top-[25vw] md:text-[4vw] md:right-[2vw]"> 
-                        CS&DS
-                    </div>
-                    
+        <>
+            <div className="relative z-0 p-3 md:p-10 min-h-screen min-w-screen">
+                <Waves
+                    lineColor="rgba(0, 0, 0, 0.2)"
+                    backgroundColor="rgba(16, 60, 101, 1)"
+                    waveSpeedX={0.02}
+                    waveSpeedY={0.01}
+                    waveAmpX={40}
+                    waveAmpY={20}
+                    friction={0.9}
+                    tension={0.01}
+                    maxCursorMove={0}
+                    xGap={12}
+                    yGap={36}
+                    style={{ pointerEvents: "none" }}>
+                </Waves>
+                <HeroSection />
+            </div>
+            <div >
+                <Card >
+                    <CardContent>
 
-                    <CardContent className="relative z-10 p-6">
                     </CardContent>
                 </Card>
             </div>
-        </div>
+        </>
     );
 };
 
