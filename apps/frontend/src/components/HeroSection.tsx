45e52360496da995c12099196d287158bbee44ee
KaitoTabor <kaitoariyasu@gmail.com>
2025-09-11 07:52:35 -0400

    state save of hero

--- apps/frontend/src/components/HeroSection.tsx
+++ apps/frontend/src/components/HeroSection.tsx
@@ -12,22 +12,22 @@ const HeroSection = () => {
                           }}>
                     </Card>
                 </div>
-                <div className="absolute opacity-0 md:opacity-100 top-[40vh] text-[8vh] font-kanji w-full flex justify-end right-[1vh] md:-top-[23vw] rotate-90 md:text-[8vw] md:left-[28.5vw]">
+                <div className="absolute  top-[35vh] left-[11vh] text-[8vh] font-kanji w-full flex justify-end right-[1vh] md:-top-[23vw] rotate-90 md:text-[8vw] md:left-[28.5vw]">
                     有安
                 </div>
-                <div className="absolute text-[4vh] font-kanji w-full flex justify-end top-[35vh] left-[18vh]  rotate-90 md:text-[8vw] md:-top-[16.5vw] md:left-[21.5vw]">
-                    Kaito
+                <div className="absolute text-[7vh] top-[35vh] right-[6vh] font-kanji w-full flex justify-end   md:text-[8vw] md:-top-[16.5vw] md:left-[21.5vw]">
+                    Kaito 
                 </div>
-                <div className="absolute opacity-0 md:opacity-100 top-[40vh] text-[8vh] font-kanji w-full flex justify-end right-[1vh] md:top-[3vw] rotate-90 md:text-[8vw] md:left-[28.5vw]">
+                <div className="absolute top-[40vh] left-[17vh] text-[8vh] font-kanji w-full flex justify-end right-[1vh] md:top-[3vw] rotate-90 md:text-[8vw] md:left-[28.5vw]">
                     Tabor
                 </div>
-                <div className="absolute text-[4vh] font-kanji w-full flex justify-end top-[35vh] left-[18vh]  rotate-90 md:text-[8vw] md:top-[0vw] md:left-[21.5vw]">
+                <div className="absolute text-[7vh] top-[40vh] right-[8vh] font-kanji w-full flex justify-end   md:rotate-90 md:text-[8vw] md:top-[0vw] md:left-[21.5vw]">
                     海飛
                 </div>
-                <div className="absolute top-[63.5vh] text-[5vh] font-kanji w-full flex justify-start left-[1vh] md:top-[38.5vw] md:text-[5vw] md:left-[1vw]">
+                <div className="absolute top-[80vh] text-[5vh] font-kanji w-full flex justify-start left-[1vh] md:top-[38.5vw] md:text-[5vw] md:left-[1vw]">
                     ポートフォリオ
                 </div>
-                <div className="absolute top-[60vh] text-[4vh] font-kanji w-full flex justify-start left-[1vh] md:top-[34.5vw] md:text-[5vw] md:left-[1.5vw]">
+                <div className="absolute top-[76vh] text-[4.5vh] font-kanji w-full flex justify-start left-[1vh] md:top-[34.5vw] md:text-[5vw] md:left-[1.5vw]">
                     Portfolio
                 </div>
                 <div className="absolute top-[30vh] text-[4vh] font-kanji w-full flex justify-end right-[2vh] text-white md:top-[30vw] md:text-[4vw] md:right-[16vw]">
